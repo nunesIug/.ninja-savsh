6ef01f1b838933da50adf4962592258c402e0972
GuilhermeNunes <digobros13@gmail.com>
2025-01-17 18:17:01 -0300

    updated

--- savsh/models.py
+++ savsh/models.py
@@ -20,13 +20,3 @@ class Meta(models.Model):
     def __str__(self):
         return self.value + ' | ' + self.description
     
-
-class Desire(models.Model):
-    id = models.AutoField(primary_key=True, unique=True)
-    name = models.CharField(max_length=40)
-    price = models.CharField(max_length=10)
-    description = models.TextField(null=True, blank=True, default="No description")
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name + ' | ' + self.price
--- savsh/schemas/DesireSchema.py
+++
@@ -1,6 +0,0 @@
-from ninja import Schema
-
-class DesireSchema(Schema):
-    name: str
-    value: str
-    description: str = None
