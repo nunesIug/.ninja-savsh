cd8f3c921e06926abeef95d5bebf8f5a702cc54c
GuilhermeNunes <digobros13@gmail.com>
2025-01-16 22:52:43 -0300

    added Meta model

--- savsh/models.py
+++ savsh/models.py
@@ -9,3 +9,24 @@ class Expense(models.Model):
     
     def __str__(self):
         return self.name + ' | ' + self.value
+    
+    
+class Meta(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    value = models.CharField(max_length=10)
+    description = models.TextField(null=True, blank=True, default="No description")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.value + ' | ' + self.description
+    
+
+class Desire(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    name = models.CharField(max_length=40)
+    price = models.CharField(max_length=10)
+    description = models.TextField(null=True, blank=True, default="No description")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + ' | ' + self.price
